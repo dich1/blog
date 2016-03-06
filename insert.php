640b78734d40a2a2638c314da903250d92bba1b1
dich1 <daich125912@gmail.com>
2016-03-07 07:46:24 +0900

    update文

--- insert.php
+++ insert.php
@@ -43,13 +43,37 @@ try{
     $stmt->execute($parms);
 
     while($result = $stmt->fetch(PDO::FETCH_ASSOC)){
+        print($result['id']);
         print($result['name']);
         print($result['email']);
         print($result['message']);
         print($result['created_at'].'<br>');
     }
 
-    
+
+	$sql = 'update contacts set name = ? where id = ?';    
+	$stmt = $dbh->prepare($sql);
+	$flag = $stmt->execute(array('updateで編集', 1));
+
+	if ($flag){
+        print('データの更新に成功しました<br>');
+    }else{
+        print('データの更新に失敗しました<br>');
+    }
+
+    print('更新後のデータ一覧：<br>');
+
+    $sql = 'select id, name, email, message, created_at from contacts';
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute($parms);
+
+    while($result = $stmt->fetch(PDO::FETCH_ASSOC)){
+        print($result['id']);
+        print($result['name']);
+        print($result['email']);
+        print($result['message']);
+        print($result['created_at'].'<br>');
+    }    
 }catch (PDOException $e){
   print('Connection failed:'.$e->getMessage());
   die();
